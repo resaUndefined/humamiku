2a9366520904b57a174718f61922cf845ee8ab95
resaUndefined <resamuhammad96@gmail.com>
2019-11-17 09:29:22 +0700

    fixing bugs on iuran and cashflow

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -121,7 +121,7 @@
                       <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">TTL <span class="required">*</span></label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input type="text" class="form-control has-feedback-left" id="single_cal3" placeholder="First Name" aria-describedby="inputSuccess2Status3" name="ttl">
+                          <input type="text" class="form-control has-feedback-left" id="single_cal3" aria-describedby="inputSuccess2Status3" name="ttl">
                           <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
                           <span id="inputSuccess2Status3" class="sr-only">(success)</span>
                         </div>
