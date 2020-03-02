ff0abb4c92d8fd84b3c8e76ff6627960c594d655
resaUndefined <resamuhammad96@gmail.com>
2020-03-02 09:14:18 +0700

    moving domain web from heroku to 000webhost

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('http://humamiku.000webhostapp.com/');
 });
 
 Auth::routes();
